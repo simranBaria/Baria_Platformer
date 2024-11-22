2551d626fdbb1528bdd47210dc448c1cb1e81f22
Simran <simranbaria06@gmail.com>
2024-11-21 19:00:40 -0500

    fixed movement

--- Platformer Project/Assets/Scripts/PlayerController.cs
+++ Platformer Project/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Facing direction
     public enum FacingDirection
     {
         left, right
     }
+    FacingDirection direction = FacingDirection.right;
 
-    FacingDirection direction;
+    // Horizontal movement
+    public float walkSpeed;
+    public float maxWalkSpeed;
+    public bool walking = false;
+    public bool decelerate = false;
+    public float acceleration;
+    public float accelerationTime;
+    float xMovement;
+
+    // Vertical movement
+    public float apexHeight;
+    public float apexTime;
+    public float initialJumpVelocity;
+    public bool jumped = false;
+    public float maxFallSpeed;
+    public float gravity;
+
+    // Grounded
+    public float groundCheck;
+    public Transform groundPosition;
+
+    // Roofed
+    public float roofCheck;
+    public Transform roofPosition;
+
     Rigidbody2D rb;
-    public float playerSpeed, maxSpeed, acceleration, accelerationTime;
-    public bool decelerate;
-    public float distanceToGround;
-    public float apexHeight, apexTime, gravity, initialJumpVelocity;
-    public bool jumping;
-    public float horizontalVelocity, verticalVelocity;
-    public Vector2 currentVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -25,109 +44,137 @@ public class PlayerController : MonoBehaviour
         // Get the rigidbody
         rb = GetComponent<Rigidbody2D>();
 
-        // Calculate acceleration
-        acceleration = maxSpeed / accelerationTime;
-
-        // Default value
-        decelerate = false;
-        direction = FacingDirection.right;
-        jumping = false;
-
-        // Get the distance to the ground
-        distanceToGround = GetDistanceToGround();
-
-        // Calculate gravity and velocity
+        // Gravity
         gravity = -2 * apexHeight / Mathf.Pow(apexTime, 2);
+
+        // Jump velocity
         initialJumpVelocity = 2 * apexHeight / apexTime;
+
+        // Walk acceleration
+        acceleration = maxWalkSpeed / accelerationTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 playerInput;
+        // Horizontal input stopped
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            decelerate = true;
+            walking = false;
+        }
 
-        // Player stopped moving
-        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A)) decelerate = true;
-
-        // Moving right
+        // Right input
         if (Input.GetKey(KeyCode.D))
         {
+            xMovement = 1;
             direction = FacingDirection.right;
-            playerInput = Vector2.right;
-            decelerate = false;
+            walking = true;
         }
-        // Moving left
+        // Left input
         else if (Input.GetKey(KeyCode.A))
         {
+            xMovement = -1;
             direction = FacingDirection.left;
-            playerInput = Vector2.left;
-            decelerate = false;
-        }
-        else playerInput = Vector2.zero;
-
-        // If the player is decelerating then keep their input direction the same
-        if (decelerate)
-        {
-            if (direction == FacingDirection.left) playerInput = Vector2.left;
-            else if (direction == FacingDirection.right) playerInput = Vector2.right;
+            walking = true;
         }
 
-        // Allow the player to jump if they're grounded
-        if (Input.GetKeyDown(KeyCode.W) && IsGrounded()) verticalVelocity = initialJumpVelocity;
+        // Vertical input
+        if (Input.GetKey(KeyCode.W) && IsGrounded()) jumped = true;
 
-        // Move the player
-        MovementUpdate(playerInput);
+        MovementUpdate(xMovement);
     }
 
-    private void MovementUpdate(Vector2 playerInput)
+    private void MovementUpdate(float xMovement)
     {
-        // Don't move if no input
-        if (playerInput != Vector2.zero || decelerate)
-        {
-            Debug.Log("test if in here");
-            // Decelerate speed
-            if (decelerate)
-            {
-                // Decelerate until stopped
-                if (playerSpeed > 0) playerSpeed -= acceleration * Time.deltaTime;
-                else
-                {
-                    playerSpeed = 0;
-                    decelerate = false;
-                }
-            }
-            // Accelerate speed
-            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
-            {
-                // Accelerate until reached max speed
-                if (playerSpeed < maxSpeed) playerSpeed += acceleration * Time.deltaTime;
-                else playerSpeed = maxSpeed;
-            }
-
-            // Move the player
-            currentVelocity = new Vector2(playerSpeed * Time.deltaTime * playerInput.x, currentVelocity.y);
-        }
+        float xChange;
+        float yChange = rb.velocity.y;
 
         // Apply gravity
-        if (verticalVelocity > gravity) verticalVelocity += gravity * Time.deltaTime;
-        else verticalVelocity = gravity;
-        currentVelocity = new Vector2(currentVelocity.x, verticalVelocity * Time.deltaTime);
+        if (!IsGrounded()) yChange += gravity * Time.deltaTime;
+
+        // Jump
+        if (jumped)
+        {
+            yChange = initialJumpVelocity;
+            jumped = false;
+        }
+
+        // Limit fall velocity
+        //if (rb.velocity.y < -maxFallSpeed) rb.velocity = new Vector2(rb.velocity.x, maxFallSpeed);
+
+        // Walk
+
+        if (walking)
+        {
+            if (walkSpeed < maxWalkSpeed) walkSpeed += acceleration * Time.deltaTime;
+            else walkSpeed = maxWalkSpeed;
+        }
+        // Decelerate
+        else if (decelerate)
+        {
+            // Decelerate until stopped
+            if (walkSpeed > 0) walkSpeed -= acceleration * Time.deltaTime;
+            else
+            {
+                walkSpeed = 0;
+                decelerate = false;
+                xMovement = 0;
+            }
+        }
+        xChange = xMovement * walkSpeed;
+
+        rb.velocity = new Vector2(xChange, yChange);
+
+        // Change visual direction
+        if (xMovement > 0) direction = FacingDirection.right;
+        else if (xMovement < 0) direction = FacingDirection.left;
+
+        //// Don't move if no input
+        //if (playerInput != Vector2.zero || decelerate)
+        //{
+        //    Debug.Log("test if in here");
+        //    // Decelerate speed
+        //    if (decelerate)
+        //    {
+        //        // Decelerate until stopped
+        //        if (playerSpeed > 0) playerSpeed -= acceleration * Time.deltaTime;
+        //        else
+        //        {
+        //            playerSpeed = 0;
+        //            decelerate = false;
+        //        }
+        //    }
+        //    // Accelerate speed
+        //    else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        //    {
+        //        // Accelerate until reached max speed
+        //        if (playerSpeed < maxSpeed) playerSpeed += acceleration * Time.deltaTime;
+        //        else playerSpeed = maxSpeed;
+        //    }
+
+        //    // Move the player
+        //    currentVelocity = new Vector2(playerSpeed * Time.deltaTime * playerInput.x, currentVelocity.y);
+        //}
+
+        //// Apply gravity
+        //if (verticalVelocity > gravity) verticalVelocity += gravity * Time.deltaTime;
+        //else verticalVelocity = gravity;
+        //currentVelocity = new Vector2(currentVelocity.x, verticalVelocity * Time.deltaTime);
 
 
-        rb.MovePosition(rb.position + currentVelocity);
+        //rb.MovePosition(rb.position + currentVelocity);
     }
 
     public bool IsWalking()
     {
-        // The player is walking if their speed is greater than 0
-        if (playerSpeed > 0) return true;
-        else return false;
+        return walking;
     }
 
     public bool IsGrounded()
     {
         // Checks if the raycast hits an object underneath the player at 0.1 distance
-        return Physics2D.Raycast(transform.position, Vector2.down, distanceToGround + 0.1f);
+        return Physics2D.Raycast(groundPosition.position, Vector2.down, groundCheck);
     }
 
     public FacingDirection GetFacingDirection()
@@ -136,9 +183,15 @@ public class PlayerController : MonoBehaviour
         return direction;
     }
 
-    public float GetDistanceToGround()
+    public bool IsRoofed()
     {
-        // Get the distance to the ground from the middle of the rigidbody
-        return Physics2D.Raycast(rb.position, Vector2.down).distance;
+        // Checks if the raycast hits an object above the player at 0.1 distance
+        return Physics2D.Raycast(roofPosition.position, Vector2.up, roofCheck);
+    }
+
+    public void FallFast()
+    {
+        // Stops the player's jump fast once they hit a roof
+        rb.velocity = new Vector2(rb.velocity.x, 0);
     }
 }
